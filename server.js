e113cd296ff5d820c919f69da714267a9ac6a297
YaKrivtsova <32088695+YaKrivtsova@users.noreply.github.com>
2022-03-02 17:35:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http=require('http');
 const fs= require('fs');
 const server=http.createServer(function(request,response){
+console.log('Cookie: ', request.cookies);	
 if (request.url==='/'){
 const text=fs.readFileSync('index.html','utf8');
 response.end(text);
