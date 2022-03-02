5201ca9b548d863e5ade38a49e2a671bc16f3570
YaKrivtsova <32088695+YaKrivtsova@users.noreply.github.com>
2022-03-02 16:45:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ response.end(text);
     });
 
          request.on ('end', function () {// После срабатывания конечного события сообщение анализируется в реальный формат запроса POST через querystring.parse, а затем возвращается клиенту.
-        post = querystring.parse(post);
 		console.log(post);
         request.end();
     });
