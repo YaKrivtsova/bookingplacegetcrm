eb7dd575cfe7b7769d00c5eb81da89e432ce3e61
YaKrivtsova <32088695+YaKrivtsova@users.noreply.github.com>
2022-03-02 17:44:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ console.log('Cookie: ', request.cookies);
 if(request.cookies=== undefined)
 {
 	console.log('you lose');
-	request.url='/';
+	response.redirect("https://bookingplacegetcrm.herokuapp.com/index.html")
 }	
 if (request.url==='/'){
 const text=fs.readFileSync('index.html','utf8');
